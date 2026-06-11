3a97cc7af479487c5ed2376efeede4e4cf349eb9
stephenduura <weseeallnfts@gmail.com>
2026-06-11 21:07:54 +0100

    Make db connection lazy

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -6,8 +6,41 @@ import { getSupabasePoolConfig } from "./supabase";
 
 const { Pool } = pg;
 
-export const pool = new Pool(getSupabasePoolConfig());
-export const db = drizzle(pool, { schema });
+let pool;
+let dbInstance;
+
+function getPool() {
+  if (!pool) {
+    pool = new Pool(getSupabasePoolConfig());
+  }
+  return pool;
+}
+
+function getDb() {
+  if (!dbInstance) {
+    dbInstance = drizzle(getPool(), { schema });
+  }
+  return dbInstance;
+}
+
+export const pool = new Proxy(
+  {},
+  {
+    get(_target, prop) {
+      return getPool()[prop];
+    },
+  },
+);
+
+export const db = new Proxy(
+  {},
+  {
+    get(_target, prop) {
+      const value = getDb()[prop];
+      return typeof value === "function" ? value.bind(getDb()) : value;
+    },
+  },
+);
 
 export { sql } from "drizzle-orm";
 export * from "./schema";
